8e4a935e2fdc80d26cdf254fcbfa0d3d8e0a86a3
Jonathan <jonathan.grangien@gmail.com>
2017-08-13 23:49:34 +0200

    Add divine image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ancientQuotes from '../data/quotes'
+import keeb from './hhkb.png'
 import './index.css'
 
 const IndexPage = () =>
+  <div>
   <div className="quotewrapper">
     <div className="quote">
       <p>{ ancientQuotes[[Math.floor(Math.random() * ancientQuotes.length)]] }.</p>
-      <div></div>
+    </div>
+  </div>
+
+    <div className="imagewrapper">
+      <img src={keeb} alt="keyboard layout"/>
     </div>
   </div>
 
