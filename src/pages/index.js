7aa30dd3366ff080f6e1b75c24b69d4702148201
Jonathan <jonathan.grangien@gmail.com>
2017-08-14 11:45:48 +0200

    Be stateful

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,55 @@ import './index.css'
 //const bgImg = divineLinks[Math.floor(Math.random() * divineLinks.length)]
 const bgImg = divineLinks[0]
 
-const IndexPage = () =>
-  <div>
-    <div style={{
-      backgroundImage: `url(${bgImg})`,
-      backgroundSize: 'cover',
-      borderBottom: '2px solid #522856',
-      minHeight: '90vh'
-    }}>
-      <div style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        paddingTop: 70,
-      }}>
-        <div className="quotewrapper">
-          <div className="quote">
-            <p>{ ancientQuotes[Math.floor(Math.random() * ancientQuotes.length)] }.</p>
+class IndexPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      quote: '',
+      footerText: ''
+    }
+  }
+
+  componentWillMount() {
+    this.setState({
+      quote: ancientQuotes[Math.floor(Math.random() * ancientQuotes.length)],
+      footerText: finalThoughts[Math.floor(Math.random() * finalThoughts.length)]
+    })
+  }
+
+  render() {
+    return (
+
+      <div>
+        <div style={{
+          backgroundImage: `url(${bgImg})`,
+          backgroundSize: 'cover',
+          borderBottom: '2px solid #522856',
+          minHeight: '90vh'
+        }}>
+          <div style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            paddingTop: 70,
+          }}>
+            <div className="quotewrapper">
+              <div className="quote">
+                <p>{this.state.quote}.</p>
+              </div>
+            </div>
+
+            <div className="imagewrapper">
+              <img src="http://i.imgur.com/AQaoTpX.png" alt="keyboard layout"/>
+            </div>
           </div>
         </div>
 
-        <div className="imagewrapper">
-          <img src="http://i.imgur.com/AQaoTpX.png" alt="keyboard layout"/>
+        <div className="footer" style={{minHeight: '10vh'}}>
+          <p>{this.state.footerText} Be at peace and return wiser.</p><br/>
         </div>
       </div>
-    </div>
-
-    <div className="footer" style={{minHeight: '10vh'}}>
-      <p>{ finalThoughts[Math.floor(Math.random() * finalThoughts.length)] } Be at peace and return wiser.</p><br/>
-    </div>
-  </div>
+    )
+  }
+}
 
 export default IndexPage
